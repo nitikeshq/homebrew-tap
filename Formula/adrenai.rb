4529ba4fde6afa37be6be99f9cd93757ba47422e
nikhilesh121 <nikhilesh@qwegle.com>
2026-06-09 18:34:49 +0530

    chore: update AdrenAI formula to 1.1.0

--- Formula/adrenai.rb
+++ Formula/adrenai.rb
@@ -1,9 +1,9 @@
 class Adrenai < Formula
   desc "Repository-aware configuration for AI coding agents"
   homepage "https://nitikeshq.github.io/adrenai/"
-  url "https://github.com/nitikeshq/adrenai/releases/download/v1.0.1/adrenai-1.0.1.tgz"
-  version "1.0.1"
-  sha256 "cf8614cd8c87e6ff58eb17b265e823c5a097a36cea64103d0d19765d0af4bcf9"
+  url "https://github.com/nitikeshq/adrenai/releases/download/v1.1.0/adrenai-1.1.0.tgz"
+  version "1.1.0"
+  sha256 "6f2a11e945576c5d9fc4210079584ca6843c2b1daa6cf87c99cb87fca7afb9a1"
   license "Apache-2.0"
 
   depends_on "node@22"
